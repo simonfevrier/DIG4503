cc7ab273db20ff0528ab0417d5367820d333bd85
simonfevrier <77869985+simonfevrier@users.noreply.github.com>
2021-01-23 00:38:13 -0500

    Delete Simon.js

--- lab1/Simon.js
+++
@@ -1,9 +0,0 @@
-const Person = require("./Person.js");
-
-class Simon extends Person {
-    constructor(name, colors, bgColors) {
-        super(name, colors, bgColors);
-    }
-}
-
-module.exports = Simon;
