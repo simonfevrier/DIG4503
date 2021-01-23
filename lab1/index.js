627fd205bae9131c56c70b9042930af53d0c53c8
simonfevrier <77869985+simonfevrier@users.noreply.github.com>
2021-01-23 00:14:10 -0500

    Create index.js

---
+++ lab1/index.js
@@ -0,0 +1,13 @@
+const S = require("./Simon.js");
+
+let p = new Rachel ("Simon", [
+    "#FF2B5D",
+    "#FF0000",
+    "#355E3B"
+], [
+    "#8BA832",
+    "#E0B0FF",
+    "#00FFFF"
+]);
+
+p.speak();
