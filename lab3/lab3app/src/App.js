179e2adabd0f9d89bd6b9888f5b2cf97ba6bcf73
simonfevrier <simonfevrier123@gmail.com>
2021-02-05 21:39:43 -0500

    Create App.js

---
+++ lab3/lab3app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import HomePage from './components/homepage/homepage'
+
+function App() {
+  const fname = 'Simon';
+  return (
+    <HomePage firstname={fname} />
+  )
+}
+
+export default App;
