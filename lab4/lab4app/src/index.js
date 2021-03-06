da176a8f4cd426782e8295a9e04346c731990d37
simonfevrier <simonfevrier123@gmail.com>
2021-03-06 00:00:19 -0500

    Update index.js

--- lab4/lab4app/src/index.js
+++ lab4/lab4app/src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
+
+class HomePage extends React.Component {
+
+  render(fname) {
+          return <h1>Market Items</h1>;
+        }
+  }
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
