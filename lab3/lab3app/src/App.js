c5ed15b278fdf76bce01bb2e778cd649d5689a0c
simonfevrier <simonfevrier123@gmail.com>
2021-03-19 17:53:15 -0400

    Update App.js

--- lab3/lab3app/src/App.js
+++ lab3/lab3app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import HomePage from './components/homepage/homepage'
 
+
 function App() {
   const fname = 'Simon';
   return (
     <HomePage firstname={fname} />
   )
 }
+
 
 export default App;
