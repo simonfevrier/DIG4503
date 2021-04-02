e4d1422d51cf31d359c700b79d445ed934c43b89
simonfevrier <simonfevrier123@gmail.com>
2021-04-02 13:39:03 -0400

    Create index.js

---
+++ lab10/index.js
@@ -0,0 +1,54 @@
+import Express from "express";
+import fs from "fs";
+import cors = from "cors";
+
+const App = Express();
+const port = 3010;
+App.use(cors());
+
+let fileContents = fs.readFileSync("database.json");
+
+let database = JSON.parse(fileContents);
+
+App.use("/", Express.static("client/build"));
+
+App.get("employees/:name", (req, res) => {
+  let result = {"error": "not found"};
+
+  database.forEach((value) => {
+    if(req.params.name == value.name) {
+      result = value;
+    }
+  });
+
+  res.json(result);
+})
+
+App.get("ages/:number", (req,res) => {
+  let result = {"error": "not found"};
+
+  database.forEach((value) => {
+    if(req.params.number == value.number) {
+      result = value;
+    }
+  });
+
+  res.json(result);
+  })
+
+App.post("employee/:name/:age", (req, res)=> {
+  let result = {
+      "name": req.params.name,
+      "age": parseINT(req.params.age)
+  };
+
+  database.push(result)
+
+  fs.writeFileSync("database.json", JSON.stringify(database, null, '\t')
+
+  res.json(result);
+});
+
+App.listen(port, () => {
+  console.log("Server running!");
+})
