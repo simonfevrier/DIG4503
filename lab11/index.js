a869a2453603aea1b616c0336ca0dd34688764ff
simonfevrier <simonfevrier123@gmail.com>
2021-04-09 20:01:53 -0400

    Create index.js

---
+++ lab11/index.js
@@ -0,0 +1,21 @@
+import MongoCLient from 'mongodb';
+
+const URL = "mongodb+srv://sfevrier:dozhaWpDIoeSaMAy@cluster0.oewwc.mongodb.net/";
+
+MongoClient.connect(URL, {useUnifiedTopology: true})
+.then(connection => {
+    let database = connection.db("sample_airbnb");
+
+    let collection = database.collection("listingAndReviews");
+
+    let cursor = collection.find({score: {$gte: "99"}, beds: {$gte: 5}, price: {$lte: 200}});
+
+    cursor.forEach(document => {
+      console.log(document.name);
+    }, () => {
+      connection.close();
+    })
+})
+.catch(error => {
+  console.log("Error: " + error);
+});
